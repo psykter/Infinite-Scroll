44585fb0e5922f7ede2d6879b1d779ba2c120c2e
Rick Diaz <richard.david.diaz@gmail.com>
2023-09-01 14:34:46 -0500

    code clean up

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let ready = false;
-let imagesLoaded = 0;
-let totalImages = 0;
-let photosArray = [];
-
 // Unsplash API
 const count = 5;
 const apiKey = 'UDS1p8CFX4ttPbgNlLRkuUvRNBqpqgJ3ybAQQzOiTEE';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
 
 // Check if all images were loaded
 function imageLoaded() {
